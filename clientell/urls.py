10062dd07805c29e73b26c31c31f4793bdbd8855
Ayush Bansal <ayushb6732@gmail.com>
2022-07-09 16:14:07 +0530

    Clientell API

--- clientell/urls.py
+++ clientell/urls.py
@@ -24,11 +24,11 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    re_path(r'^media/(?P<path>.*)$', serve,
-            {'document_root':       settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve,
-            {'document_root': settings.STATIC_ROOT}),
+    # re_path(r'^media/(?P<path>.*)$', serve,
+    #         {'document_root':       settings.MEDIA_ROOT}),
+    # re_path(r'^static/(?P<path>.*)$', serve,
+    #         {'document_root': settings.STATIC_ROOT}),
 ]
 
-urlpatterns = urlpatterns + \
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns = urlpatterns + \
+#     static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.index, name="index"),
+    path('', views.index, name="index"),
     path('docs', views.docs, name="docs"),
     path('docs/opportunity', views.opportunityDocs, name="opportunityDocs"),
     path('docs/accounts', views.accountsDocs, name="accountsDocs"),
