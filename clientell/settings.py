677da666113448c9783c92cdca0f18978e6cd318
Ayush Bansal <ayushb6732@gmail.com>
2022-07-09 16:28:52 +0530

    Clientell API

--- clientell/settings.py
+++ clientell/settings.py
@@ -135,7 +135,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'home/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Default primary key field type
