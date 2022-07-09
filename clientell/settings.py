e90493d73d650ddd2862822dde509c8f61c1521e
Ayush Bansal <ayushb6732@gmail.com>
2022-07-09 15:32:11 +0530

    Clientell API

--- clientell/settings.py
+++ clientell/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
-
+SECRET_KEY = 'django-insecure-k9ti08a@@ng&wqo6pm@k^nq%7v6bw_=#==mca)sdx*z!^_5l$e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
